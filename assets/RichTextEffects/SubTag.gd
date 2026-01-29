50fcbbb7c1da135342e8bf62e4d3703458e3d2fd
Levonze <raupenzelevonze@gmail.com>
2026-01-29 19:49:39 +0500

    Added console

--- assets/RichTextEffects/SubTag.gd
+++ assets/RichTextEffects/SubTag.gd
@@ -5,7 +5,9 @@ class_name RichTextSub
 var bbcode = "sub"
 
 func _process_custom_fx(char_fx):
+	var x = char_fx.env.get("x", 2)
+	var y = char_fx.env.get("y", 4)
 	var scale = .7
 	char_fx.transform = char_fx.transform.scaled_local(Vector2(scale, scale))
-	char_fx.offset = Vector2(2, 4)
+	char_fx.offset = Vector2(x,y)
 	return true
